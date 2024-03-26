8ec5964e8f76046b9b93b3de1387612588a3f9fe
dargven <42145310+dargven@users.noreply.github.com>
2024-03-26 19:33:42 +0400

    minor

--- main.py
+++ main.py
@@ -1,40 +1,60 @@
+import random
+import time
+import base64
+
+import requests
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.devtools.v122.network import Cookie
 from selenium.common.exceptions import NoSuchElementException
-import time
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import base64
 
 driver = webdriver.Chrome()
 
 
 def agree_with_google():
     driver.get('https://google.com/xhtml')
-
     time.sleep(2)  # seconds until popup appears
-
     try:  # 2 different popups
         frame = driver.find_element(By.XPATH, '//*[@id="cnsw"]/iframe')  # <-locating chrome cookies consent frame
-        driver.switch_to.frame(frame)
-        driver.find_element(By.XPATH,
-                            '//*[@id="introAgreeButton"]').click()  # <-looking for introAgreeButton button, but seems google has changed its name since and  it only works in old chrome versions.
-
+        if frame is not None:
+            driver.switch_to.frame(frame)
+            button = driver.find_element(By.XPATH, '//*[@id="introAgreeButton"]')
+            if button:
+                button.click()
     except NoSuchElementException:
-        driver.find_element(By.XPATH, '//*[@id="L2AGLb"]').click()
+        button = driver.find_element(By.XPATH, '//*[@id="L2AGLb"]')
+        if button:
+            button.click()
 
 
-def search_images():
+def saved_images_to_download(images, count, filtered_images):
+    for image in images:
+        src = image.get_attribute('src')
+        if (src is not None) and (src not in filtered_images):
+            filtered_images.append(src)
+            if len(filtered_images) >= count:
+                break
+        else:
+            continue
+
+
+def search_images(count=1000):
+    filtered_images = []
     search_term = "car side view"
     url = (f"https://www.google.com/search?q={search_term}&source=lnms&tbm=isch&sa=X&ved"
            f"=2ahUKEwie44_AnqHpAhUhBWMBHUFGD90Q_AUoAXoECBUQAw&biw=1920&bih=947")
     driver.get(url)
-    wait = WebDriverWait(driver, 5)
+    wait = WebDriverWait(driver, 1)
     wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'rg_i')))
-    elements = (driver.find_elements(By.CLASS_NAME, 'rg_i'))
-    return [element.get_attribute("src") for element in elements]
+    while len(filtered_images) < count:
+        time.sleep(2)
+        images = driver.find_elements(By.CLASS_NAME, 'rg_i')
+        saved_images_to_download(images, count, filtered_images)
+        time.sleep(4)
+        scroll_page()
+    return filtered_images
 
 
 def scroll_page():
@@ -43,18 +63,30 @@ def scroll_page():
 
 def download_images(images_urls):
     save_images = 0
-
-    while save_images < len(images_urls):
-        src = images_urls[save_images]
-        if src and ',' in src:
-            head, data = src.split(',', 1)
-            file_ext = '.' + head.split(';')[0].split('/')[1]
-            plain_data = base64.b64decode(data)
-            save_path = 'images/image'
-
-            with open(save_path + str(save_images) + file_ext, "wb") as file:
-                file.write(plain_data)
-                save_images += 1
+    print(len(images_urls))
+    # print(save_images)
+    #
+    # for url in images_urls:
+    #     if url.startswith('data:image'):
+    #         head, data = url.split(',', 1)
+    #         file_ext = '.' + head.split(';')[0].split('/')[1]
+    #         plain_data = base64.b64decode(data)
+    #         save_path = f'src/images/image{save_images}{file_ext}'
+    #         with open(save_path, "wb") as file:
+    #             file.write(plain_data)
+    #             save_images += 1
+    #     else:
+    #         try:
+    #             response = requests.get(url)
+    #             if response.status_code == 200:
+    #                 content_type = response.headers.get('content-type')
+    #                 file_ext = '.' + content_type.split('/')[-1]
+    #                 save_path = f'src/images/image{save_images}{file_ext}'
+    #                 with open(save_path, 'wb') as f:
+    #                     f.write(response.content)
+    #                     save_images += 1
+    #         except Exception as e:
+    #             print(f"Error downloading image from {url}: {e}")
 
 
 agree_with_google()
